240b46fe8b081efd3ab49438cd7747958248b85e
lhadri <el.hadri.b.adil@gmail.com>
2023-02-11 10:55:07 +0100

    done: web forms

--- hello.py
+++ hello.py
@@ -14,14 +14,42 @@ from flask_moment import Moment
 from datetime import datetime
 
 
+# Form ------------------------------------------------------------------
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+class NameForm(FlaskForm):
+   name = StringField('What is your name?', validators=[DataRequired()])
+   submit = SubmitField('Submit')
+# ------------------------------------------------------------------
+
+from flask import flash
+
 
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
+
 moment = Moment(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-   return render_template('index.html', current_time=datetime.utcnow())
+   name = None
+   form = NameForm()
+
+   # this portion is for handling the form -------------------------------
+   if form.validate_on_submit():
+      session['name'] = form.name.data
+      form.name.data = ''
+      print(name)
+      flash('Change submitted!') # we have to choose where we can display it in index.html
+
+      return redirect(url_for('index'))
+   # ---------------------------------------------------------------------
+
+   return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
--- mve/bin/activate.fish
+++ mve/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/lhadri/flasky/myflaskvenv"
+set -gx VIRTUAL_ENV "/home/lhadri/flask_tuto/mve"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(myflaskvenv) "            
-            printf "%s%s" "(myflaskvenv) " (set_color normal)
+        if test -n "(mve) "            
+            printf "%s%s" "(mve) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
